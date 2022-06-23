6329229c7e93469d1184d671068a1c04e4140ad4
at0x0 <107781869+at0x0@users.noreply.github.com>
2022-06-23 13:13:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ server = client.get_guild("556557388161875975")
 app = web.Application()
 routes = web.RouteTableDef()
 
-
 def setup(bot):
     bot.add_cog(Webserver(bot))
 
@@ -31,30 +30,14 @@ class Webserver(commands.Cog):
         async def welcome(request):
             return web.Response(text="Hello, world")
 
-        @routes.post('/dbl')
-        async def dblwebhook(request):
-            if request.headers.get('authorization') == '3mErTJMYFt':
-                data = await request.json()
-                user = self.bot.get_user(data['user']) or await self.bot.fetch_user(data['user'])
-                if user is None:
-                    return
-                _type = f'Tested!' if data['type'] == 'test' else f'Voted!'
-                upvoted_bot = f'<@{data["bot"]}>'
-                embed = discord.Embed(title=_type, colour=discord.Color.blurple())
-                embed.description = f'**Upvoter :** {user.mention} Just {_type}' + f'\n**Upvoted Bot :** {upvoted_bot}'
-                embed.set_thumbnail(url=user.avatar_url)
-                channel = self.bot.get_channel(5645646545142312312)
-                await channel.send(embed=embed)
-            return 200
-
-        self.webserver_port = os.environ.get('PORT')
+        self.webserver_port = os.environ.get('PORT', 5000)
         app.add_routes(routes)
 
     @tasks.loop()
     async def web_server(self):
         runner = web.AppRunner(app)
         await runner.setup()
-        site = web.TCPSite(runner, host='https://olam404.herokuapp.com', port=self.webserver_port)
+        site = web.TCPSite(runner, host='0.0.0.0', port=self.webserver_port)
         await site.start()
 
     @web_server.before_loop
